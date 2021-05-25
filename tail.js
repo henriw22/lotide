5d0baafa14cbf7546fc0956fe757cc227b20d4c4
henriw22 <henriw22@gmail.com>
2021-05-25 01:28:45 +0000

    a function called tail that will return an array without the element at index 0

---
+++ tail.js
@@ -0,0 +1,30 @@
+const tail = function(data) {
+  return data.slice(1);
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: ...
+// ...
+
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+// let newWords = tail(words);
+// console.log(newWords);
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
