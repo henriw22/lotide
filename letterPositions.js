eef1e228084cc9279c40b98cf4be2bdfca817cb3
henriw22 <henriw22@gmail.com>
2021-05-27 00:02:36 +0000

    a function to return the index of where the letter is on a sentence

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("✅✅✅ Assertion Passed:", arr1, "===", arr2);
+  } else {
+    console.log("🔴🔴🔴 Assertion Failed:", arr1, "!==", arr2);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+
+const letterPositions = function(sentence) {
+  const result = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (result[sentence[i]]) {
+        result[sentence[i]].push(i);
+      } else {
+        result[sentence[i]] = [i];
+      }
+    }
+  }
+  // console.log(result);
+  return result;
+};
+
+
+letterPositions("lighthouse lab lullaby");
+console.log(letterPositions("lighthouse lab lullaby"));
+
+assertArraysEqual(letterPositions("lighthouse lab lullaby")["l"], [0, 11, 15, 17, 18]);
+assertArraysEqual(letterPositions("lighthouse lab lullaby")["h"], [3, 5]);
+assertArraysEqual(letterPositions("lighthouse lab lullaby")["a"], [12, 19]);
