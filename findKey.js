42c3e2720cb87a23990c79b233de999f2d762c1f
henriw22 <henriw22@gmail.com>
2021-05-28 00:53:41 +0000

    function to search through the array based on the callback function and return the key

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  const keys = Object.keys(object);
+  // console.log(keys);
+  // console.log(object[keys[1]].stars);
+  for (let key of keys) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+
+assertEqual(result, "noma");
