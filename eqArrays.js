61063cfa34abb31499958602ef43288a20c2faa6
henriw22 <henriw22@gmail.com>
2021-05-31 19:43:37 +0000

    refactored file to export function and separated the test cases into a new file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,6 +11,4 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
