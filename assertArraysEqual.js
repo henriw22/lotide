afa30b73d5ba2e6eef3702af92e74d43e3138504
henriw22 <henriw22@gmail.com>
2021-05-31 19:58:39 +0000

    refactored file to export function and separated the test cases into a new file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log("✅✅✅ Assertion Passed:", arr1, "===", arr2);
@@ -6,17 +8,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
 
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+module.exports = assertArraysEqual;
